4c6d004887cd7704f92aaf5117d824571cac92f6
Álex Bachinin <bachininalexander@gmail.com>
2020-12-12 21:26:16 +0200

    Áll in one place

---
+++ something.py
@@ -0,0 +1,56 @@
+
+
+import turtle
+
+turtle.speed(7)
+turtle.setup(640, 480)
+turtle.penup()
+turtle.goto(0, -50)
+turtle.pendown()
+
+print('It is draving the cube in circle')
+
+turtle.pencolor('blue')
+turtle.bgcolor('yellow')
+
+turtle.pensize(5)
+turtle.circle(120)
+
+turtle.penup()
+turtle.goto(0, -15)
+turtle.pendown()
+
+turtle.forward(50)
+turtle.left(45)
+turtle.forward(25)
+
+turtle.penup()
+turtle.forward(25)
+turtle.pendown()
+
+turtle.left(45)
+turtle.forward(50)
+
+turtle.forward(50)
+turtle.left(45)
+turtle.forward(50)
+turtle.left(45)
+turtle.forward(50)
+
+turtle.forward(50)
+turtle.left(45)
+turtle.forward(50)
+turtle.left(45)
+turtle.forward(50)
+
+turtle.forward(50)
+turtle.left(45)
+turtle.forward(50)
+turtle.left(45)
+turtle.forward(50)
+
+turtle.pos()
+
+turtle.hideturtle()
+
+turtle.done()  		
